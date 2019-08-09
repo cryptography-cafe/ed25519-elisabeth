56796493e6f50762c178c4f8936d8ff1ee583233
Jack Grigg <thestr4d@gmail.com>
2019-08-09 22:58:23 +0100

    Validate input length to Ed25519Signature.fromByteArray()

--- src/main/java/cafe/cryptography/ed25519/Ed25519Signature.java
+++ src/main/java/cafe/cryptography/ed25519/Ed25519Signature.java
@@ -31,6 +31,10 @@ public class Ed25519Signature {
      * @return a signature.
      */
     public static Ed25519Signature fromByteArray(byte[] input) {
+        if (input.length != 64) {
+            throw new IllegalArgumentException("signature length is wrong");
+        }
+
         // RFC 8032, section 5.1.7:
         // @formatter:off
         // 1. To verify a signature [...], first split the signature into two
